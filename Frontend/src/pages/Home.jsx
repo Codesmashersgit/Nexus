b24b662b341a86e896046b68572865b27dcfa80f
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-10-08 01:49:16 +0530

    added something

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,20 +17,20 @@ function Home() {
 
   return (
     
-      <div className="">
-          <div className="h-screen flex flex-col md:flex-row z-30">
+  
+          <div className="flex flex-col md:flex-row">
   {/* Text Section */}
-  <div className="flex-1 flex flex-col lg:items-center py-28 text-center px-4">
+  <div className="h-[90vh] flex-1 flex flex-col lg:items-center lg:justify-center py-28 mt-16 text-center px-4 bg-gradient-to-r from-[#ffc6d0] to-[#ffdae0]">
     <h1 className="text-3xl md:text-[70px] font-bold leading-tight">
-      Video <span className="text-[#07f2ee]">Calls & Meetings</span> For Everyone
+      Video <span className="text-[#fa1239]">Calls & Meetings</span> For Everyone
     </h1>
-    <p className="mt-6 text-lg md:text-xl font-light">
+    <p className="mt-6 text-lg md:text-xl font-light leading-8">
       Connect, Collaborate & Celebrate with Chroma Meet.
     </p>
     <button
       type="button"
       onClick={handleClick}
-      className="border border-[#07f2ee] bg-[#07f2ee] rounded-[20px] py-2 px-6  text-[#1E1E1E] font-medium mt-5 tracking-tight"
+      className="border border-[#fa1239] rounded-[20px] py-2 px-6  text-[#1E1E1E] font-medium mt-5 tracking-tight"
     >
       Start Now
     </button>
@@ -45,15 +45,14 @@ function Home() {
               className="w-full max-w-[400px] md:max-w-[600px] h-[500px] object-contain"
             />
           </div> */}
-          <div className="p-5 absolute top-[50%] lg:hidden justify-center items-center">
+          {/* <div className="p-5 absolute top-[50%] lg:hidden justify-center items-center">
             <img
               src="https://img.freepik.com/premium-vector/illustration-cartoon-female-user-entering-login_241107-682.jpg"
               alt="Hero"
               className="w-full max-w-[400px] md:max-w-[600px] h-auto object-contain"
             />
-          </div>
+          </div> */}
         </div>
-      </div>
   );
 }
 
