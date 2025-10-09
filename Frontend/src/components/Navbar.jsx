6075594c159208e6830a309955eece124f34ced1
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-10-09 22:47:13 +0530

    Navbar with some extra functionality

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -22,7 +22,8 @@ function Navbar() {
   }, []);
 
   function handleDashboardClick() {
-    if (username ) {
+    const token = localStorage.getItem("authToken");
+    if (username && token ) {
       navigate("/dashboard");
     }
     else{
@@ -36,28 +37,30 @@ function Navbar() {
   }
 
   return (
-    <nav className="fixed md:top-0 md:px-5 top-0 px-2 w-full shadow-lg rounded">
-      <div className="mx-auto flex justify-around items-center px-6 py-3 z-50 ">
-        <div className="lg:text-3xl md:text-xl text-[20px] font-bold md:mb-0 flex items-center gap-2 justify-center">
+    <nav className="fixed md:top-0 md:px-5 top-0 px-2 w-full shadow-lg rounded z-50 bg-white ">
+      <div className="mx-auto flex lg:justify-around items-center justify-between px-6 py-3 ">
+        
+       <Link to="/"><div className="lg:text-3xl md:text-xl text-[20px] font-bold md:mb-0 flex items-center gap-2 justify-center">
           <img src={img}alt="" className="w-[51px]"></img>
           <h1 className="lg:text-[30px] font-bold">Ne<span className="text-[#fa1239]">xu</span>s</h1>
         </div>
+        </Link>
 
         <div className="md:hidden text-2xl cursor-pointer" onClick={toggleMenu}>
           {menuOpen ? <IoCloseSharp /> : <CiMenuFries />}
         </div>
 
         <ul className="hidden md:flex items-center space-x-7 font-medium">
-          <li><Link to="/" className="hover:border-b-2 border-[#07f2ee]">Home</Link></li>
-          <li><Link to="/analytics" className="hover:border-b-2 border-[#07f2ee]">Analytics</Link></li>
-          <li><Link to="/pricing" className="hover:border-b-2 border-[#07f2ee]">Pricing</Link></li>
+          <li><Link to="/" className="hover:border-b-2 border-[#fa1239]">Home</Link></li>
+          <li><Link to="/analytics" className="hover:border-b-2 border-[#fa1239]">Analytics</Link></li>
+          <li><Link to="/pricing" className="hover:border-b-2 border-[#fa1239]">Pricing</Link></li>
         </ul>
 
         {/* Username or Dashboard Button (Desktop) */}
         <div className="hidden md:block">
           <button
             onClick={handleDashboardClick}
-            className="border border-[#07f2ee] rounded-[27px] py-1 px-4 text-[#1E1E1E] font-medium shadow hover:scale-105 transition-all"
+            className="border border-[#fa1239] bg-[#fa1239] rounded-[27px] py-1 px-4 text-[#fff] font-medium shadow hover:scale-105 transition-all"
           >
             {username ? username : "Dashboard"}
           </button>
@@ -79,7 +82,7 @@ function Navbar() {
                 toggleMenu();
                 handleDashboardClick();
               }}
-              className="border border-[#07f2ee] rounded-[20px] py-1 px-4 font-medium shadow hover:scale-105 transition-all"
+              className="border border-[#fa1239] bg-[#fa1239] rounded-[20px] py-2 px-5 text-[#fff] font-medium text-[18px] shadow hover:scale-105 transition-all"
             >
               {username ? username : "Dashboard"}
             </button>
