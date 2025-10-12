965bde731ecdb2d9d3a95e725d0be72a2e1fe3f4
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-10-12 17:20:41 +0530

    updated tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,9 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'grid-pattern': `repeating-linear-gradient(
+          to right,
+          #cbd5e1 0px,
+          #cbd5e1 2px,
+          transparent 2px,
+          transparent 40px
+        ),
+        repeating-linear-gradient(
+          to bottom,
+          #cbd5e1 0px,
+          #cbd5e1 2px,
+          transparent 2px,
+          transparent 40px
+        )`,
+      },
+    },
   },
   plugins: [],
 }
-// tailwind.config.js
-
