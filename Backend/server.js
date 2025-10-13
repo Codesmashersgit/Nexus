47d6513d3ada8cc2bca4ae7bd815a60efefd09e1
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-10-13 22:09:38 +0530

    Remove unwanted code in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,12 +22,6 @@ app.use(passport.initialize());
 
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, '../Frontend/dist')));
-
-
-app.get('*wildcard', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
-});
 
 const server = http.createServer(app);
 const io = new Server(server, {
