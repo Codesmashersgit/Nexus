c6955f9e7b906894e124589e8efe8374eac1141c
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-11-01 10:08:36 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const authRoutes = require("./Routes/authRoute");
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.VITE_CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
