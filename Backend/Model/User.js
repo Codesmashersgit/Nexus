fedc6acc2303c9f8e3a5a95fec2cf7341db1173c
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-11-16 23:22:53 +0530

    Updated User.js

--- Backend/Model/User.js
+++ Backend/Model/User.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, unique: true, sparse: true },
     password: { type: String },
     googleId: { type: String },
     resetPasswordToken: String,
