96768e0280a7e1b8d319d2bf9066bede8c9fd839
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-11-16 22:55:56 +0530

    Updated AuthContext.jsx

--- Frontend/src/AuthContext.jsx
+++ Frontend/src/AuthContext.jsx
@@ -1,28 +1,39 @@
+
+
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null); 
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
-    setIsLoggedIn(!!token);
+    const username = localStorage.getItem("username");
+
+    if (token && username) {
+      setIsLoggedIn(true);
+      setUser({ name: username}); 
+    }
   }, []);
 
-  const login = (token) => {
+  const login = ({ token, username }) => {
     localStorage.setItem("authToken", token);
+    localStorage.setItem("username", username);
     setIsLoggedIn(true);
+    setUser({ name: username});
   };
 
   const logout = () => {
     localStorage.removeItem("authToken");
     localStorage.removeItem("username");
     setIsLoggedIn(false);
+    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
