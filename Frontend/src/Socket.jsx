0c98b87825932bbdf497b66e98233cf1b9151afe
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-11-16 22:54:00 +0530

    Deleted Socket.jsx

--- Frontend/src/Socket.jsx
+++
@@ -1,4 +0,0 @@
-import { io } from "socket.io-client";
-
-const SERVER_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
-export const socket = io(SERVER_URL, { transports: ["websocket"] });
