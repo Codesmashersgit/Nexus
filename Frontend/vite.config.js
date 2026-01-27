d371af0412a0a552dab318569b94369a22cdf446
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2026-01-27 16:31:08 +0530

    Updated vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react()],
+//   define: {
+//     global: 'window'
+//   }
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  define: {
-    global: 'window'
-  }
-})
+  server: {
+    host: true, // 0.0.0.0 pe listen kare
+    port: 5173,
+    strictPort: true,
+    cors: true,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      ".trycloudflare.com" // <-- Ye line important hai
+    ],
+  },
+});
