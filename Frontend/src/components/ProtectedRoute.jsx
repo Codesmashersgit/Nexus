35d27fc4ca6184f79707974bcc0b9b65863c556a
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2026-01-31 09:48:03 +0530

    modified protectedRoute

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,16 +1,17 @@
 
 import React, { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
 
 const ProtectedRoute = ({ children }) => {
     const { isLoggedIn } = useContext(AuthContext);
+    const location = useLocation();
 
     // Check if token exists in localStorage as an extra precaution or if the context hasn't loaded yet
     const token = localStorage.getItem("authToken");
 
     if (!isLoggedIn && !token) {
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/login" replace state={{ from: location }} />;
     }
 
     return children;
